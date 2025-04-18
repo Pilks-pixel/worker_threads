c7ca1f40ec98a99aa8ebb9a93d7d2ed7b06c0a78
Pilks-pixel <peterpilkington@hotmail.com>
2025-04-18 22:57:39 +0100

    added workers

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,13 @@
 import { isMainThread, Worker, workerData } from "node:worker_threads";
-
 const __filename = new URL(import.meta.url).pathname;
 console.log(__filename);
-
 if (isMainThread) {
   new Worker(__filename, { workerData: [5, 4, 3, 2, 1] });
   new Worker(__filename, { workerData: [8, 4, 7, 2, 11] });
   console.log(`Main Thread! Process ID ${process.pid}`);
 } else {
   console.log(
-    `Inside Worker! Process ID ${process.pid} & Worker Data: ${workerData} sorted: ${workerData.sort((a: number, b: number) => a - b)}`,
+    `Inside Worker! Process ID ${process.pid} & Worker Data: ${workerData} sorted: ${workerData.sort((a, b) => a - b)}`,
   );
   console.log(isMainThread); // Prints 'false'.
 }
